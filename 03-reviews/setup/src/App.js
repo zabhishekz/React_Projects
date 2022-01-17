d577a2cc93c126b55e52ec447ed8238993f3a135
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-17 16:35:11 +0530

    Reviews Project Setup And React Icons

---
+++ 03-reviews/setup/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Review from "./Review";
+import { FaGithubSquare } from "react-icons/fa";
+
+function App() {
+  return (
+    <h2>
+      reviews project setup
+      <FaGithubSquare className="icon" />
+    </h2>
+  );
+}
+
+export default App;
