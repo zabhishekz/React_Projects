c6337f261599c8c2031ad88ffbee892b5bcfd41a
Abhishek Verma <zabhishek.verma@gmail.com>
2022-02-01 22:43:22 +0530

    Grocery Bud Clear List

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -22,6 +22,7 @@ function App() {
       // deal with edit
     } else {
       // show alert
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -30,6 +31,11 @@ function App() {
 
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
   };
 
   return (
@@ -53,7 +59,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
