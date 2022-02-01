5fada5655e4ddae38e76915388e9e584bf0a3e81
Abhishek Verma <zabhishek.verma@gmail.com>
2022-02-01 13:03:38 +0530

    Lorem Ipsum Complete

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import data from "./data";
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
-
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
@@ -20,19 +28,12 @@ function App() {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button type="submit" className="btn">
-          generate
-        </button>
+        <button className="btn">generate</button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
