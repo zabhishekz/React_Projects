9d001676c18900a1c90968d554908d604a21948f
Abhishek Verma <zabhishek.verma@gmail.com>
2022-02-12 22:24:21 +0530

    Image correction

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -17,12 +17,12 @@ const AppProvider = ({ children }) => {
       const { drinks } = data;
       if (drinks) {
         const newCocktails = drinks.map((item) => {
-          const { idDrink, strDrink, strDrinkthumb, strAlcoholic, strGlass } =
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
             item;
           return {
             id: idDrink,
             name: strDrink,
-            image: strDrinkthumb,
+            image: strDrinkThumb,
             info: strAlcoholic,
             glass: strGlass,
           };
